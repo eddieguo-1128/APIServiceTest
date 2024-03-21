24bed70673c52ac4bf667a45bb519b3a9210d1b5
Eddie <eddieguo@berkeley.edu>
2024-03-21 03:11:36 -0400

    port

--- potato/flask_server.py
+++ potato/flask_server.py
@@ -2864,7 +2864,8 @@ def run_server(args):
     flask_logger = logging.getLogger("werkzeug")
     flask_logger.setLevel(logging.ERROR)
 
-    port = args.port or config.get("port", default_port)
+    port = os.environ.get('PORT',8000)
+    #port = args.port or config.get("port", default_port)
     #print("running at:\nlocalhost:" + str(port))
     app.run(debug=False, host="0.0.0.0", port=port)
 
